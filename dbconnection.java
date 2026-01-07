28781b03baebf40cd59ddfa543d93f84b58dfeca
Deepak30106 <2420030106@klh.edu.in>
2026-01-07 08:25:15 +0530

    Create dbconnection.java

---
+++ dbconnection.java
@@ -0,0 +1,20 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class dbconnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/hibernatedb?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "eurb4zwd5z";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            throw new RuntimeException("Database connection failed", e);
+        }
+    }
+}
